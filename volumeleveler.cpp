4b6cd0b447fecf28353272b24c2f456ff24bf93e
Lolirofle <1522957+Lolirofle@users.noreply.github.com>
2025-04-15 22:45:13 +0200

    Update volumeleveler lib using radiocicletta/vlevel

--- volumeleveler.cpp
+++ volumeleveler.cpp
@@ -1,20 +1,21 @@
-// Copyright 2003 Tom Felker
+// This file is part of VLevel, a dynamic volume normalizer.
 //
-// This file is part of VLevel.
+// Copyright 2003 Tom Felker <tcfelker@mtco.com>
 //
-// VLevel is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
 //
-// VLevel is distributed in the hope that it will be useful, but
+// This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with VLevel; if not, write to the Free Software Foundation,
-// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
 
 // volumeleveler.cpp - defines the VolumeLeveler class
 
@@ -26,137 +27,160 @@
 
 using namespace std;
 
-VolumeLeveler::VolumeLeveler(size_t l, size_t c, value_t s, value_t m) : m_bufs(NULL)
+VolumeLeveler::VolumeLeveler(size_t l, size_t c, value_t s, value_t m) : bufs(NULL)
 {
-    SetSamplesAndChannels(l, c);
-    SetStrength(s);
-    SetMaxMultiplier(m);
+	SetSamplesAndChannels(l, c);
+	SetStrength(s);
+	SetMaxMultiplier(m);
 }
 
 VolumeLeveler::~VolumeLeveler()
 {
-    if (m_bufs) {
-        for (size_t ch = 0; ch < m_channels; ++ch) delete[] m_bufs[ch];
-        delete[] m_bufs;
-    }
+	if(bufs) {
+		for(size_t ch = 0; ch < channels; ++ch)
+			delete[] bufs[ch];
+		delete[] bufs;
+	}
 }
 
 void VolumeLeveler::SetStrength(value_t s)
 {
-    m_strength = s;
+	strength = s;
 }
 
 void VolumeLeveler::SetMaxMultiplier(value_t m)
 {
-    if (m <= 0) m = HUGE_VAL;
-    m_max_multiplier = m;
+	if(m <= 0) m = HUGE_VAL;
+	max_multiplier = m;
 }
 
 void VolumeLeveler::SetSamplesAndChannels(size_t s, size_t c)
 {
-    if (m_bufs) {
-        for (size_t ch = 0; ch < m_channels; ++ch) delete[] m_bufs[ch];
-        delete[] m_bufs;
-        m_bufs = NULL;
-    }
+	if(bufs) {
+		for(size_t ch = 0; ch < channels; ++ch)
+			delete[] bufs[ch];
+		delete[] bufs;
+		bufs = NULL;
+	}
 
-    if (c > 0) {
-        m_bufs = new value_t*[c];
-        for (size_t ch = 0; ch < c; ++ch) m_bufs[ch] = new value_t[s];
-    }
+	if(c > 0) {
+		bufs = new value_t*[c];
+		for(size_t ch = 0; ch < c; ++ch)
+			bufs[ch] = new value_t[s];
+	}
 
-    m_samples = s;
-    m_channels = c;
-    Flush();
+	samples = s;
+	channels = c;
+	Flush();
 }
 
 void VolumeLeveler::Flush()
 {
-    if (m_bufs) {
-        for (size_t ch = 0; ch < m_channels; ++ch) {
-            for (size_t i = 0; i < m_samples; ++i) {
-                m_bufs[ch][i] = 0;
-            }
-        }
-    }
+	if(bufs) {
+		for(size_t ch = 0; ch < channels; ++ch) {
+			for(size_t i = 0; i < samples; ++i) {
+				bufs[ch][i] = 0;
+			}
+		}
+	}
 
-    m_silence = m_samples;
-    m_pos = m_max_slope_pos = 0;
-    m_max_slope = m_max_slope_val = m_avg_amp = 0;
+	silence = samples;
+	pos = max_slope_pos = 0;
+	max_slope = max_slope_val = avg_amp = 0;
 }
 
 value_t VolumeLeveler::GetMultiplier()
 {
-    value_t multiplier = (value_t)pow(m_avg_amp, -m_strength);
-    if (multiplier > m_max_multiplier) multiplier = m_max_multiplier;
-    return multiplier;
+	value_t multiplier = VLEVEL_POW(avg_amp, -strength);
+	if(multiplier > max_multiplier) multiplier = max_multiplier;
+	return multiplier;
 }
 
 size_t VolumeLeveler::Exchange(value_t **in_bufs, value_t **out_bufs, size_t in_samples)
 {
-    Exchange_n(in_bufs, out_bufs, in_samples);
+	Exchange_n(in_bufs, out_bufs, in_samples);
 
-    if (m_silence >= in_samples) {
-        m_silence -= in_samples;
-        return in_samples;
-    } else {
-        size_t returned_silence = m_silence;
-        m_silence = 0;
-        return returned_silence;
-    }
+	if(silence >= in_samples) {
+		silence -= in_samples;
+		return in_samples;
+	} else {
+		size_t returned_silence = silence;
+		silence = 0;
+		return returned_silence;
+	}
 }
 
 void VolumeLeveler::Exchange_n(value_t **in_bufs, value_t **out_bufs, size_t in_samples)
 {
-    // for each user_pos in user_buf
-    for (size_t user_pos = 0; user_pos < in_samples; ++user_pos) {
-        // compute multiplier
-        value_t multiplier = (value_t)pow(m_avg_amp, -m_strength);
-        if (multiplier > m_max_multiplier) multiplier = m_max_multiplier;
+	// for each user_pos in user_buf
+	for (size_t user_pos = 0; user_pos < in_samples; ++user_pos) {
+		// compute multiplier
+		value_t multiplier = VLEVEL_POW(avg_amp, -strength);
 
-        // swap buf[pos] with user_buf[user_pos], scaling user[buf] by
-        // multiplier and finding max of the new sample
-        value_t new_val = 0;
-        for (size_t ch = 0; ch < m_channels; ++ch) {
-            value_t in = in_bufs[ch][user_pos];
-            out_bufs[ch][user_pos] = m_bufs[ch][m_pos] * multiplier;
-            m_bufs[ch][m_pos] = in;
-            if (VLEVEL_ABS(in) > new_val) new_val = (value_t)fabs(in);
-        }
+		// if avg_amp <= 0, then the above line sets multiplier to Inf, so
+		// samples will scale to Inf or NaN.  This causes a tick on the
+		// first sample after a Flush() unless max_multiplier is not Inf.
+		// hopefully this fix isn't too slow.
+		if(avg_amp <= 0) multiplier = 0;
 
-        m_pos = (m_pos + 1) % m_samples; // now pos is the oldest, new one is pos-1
+		// untested!
+		// The advantage of using floats is that you can be
+		// sloppy with going over 1.  Since we have this nifty
+		// average_amp calculation, let's apply it to limit
+		// the audio to varying normally below 1.  Again,
+		// hopefully this won't slow things down too much.
+		if(avg_amp > 1) multiplier = 1 / avg_amp;
 
-        m_avg_amp += m_max_slope;
+		// limit multiplier to max_multiplier.  max_multiplier can be Inf
+		// to disable this.
+		if(multiplier > max_multiplier) multiplier = max_multiplier;
 
-        if (m_pos == m_max_slope_pos) {
-            // recompute (this is expensive)
-            m_max_slope = -HUGE_VAL;
-            for (size_t i = 1; i < m_samples; ++i) {
-                value_t sample_val = 0;
-                for (size_t ch = 0; ch < m_channels; ++ch) {
-                    value_t ch_val = VLEVEL_ABS(m_bufs[ch][(m_pos + i) % m_samples]);
-                    if (ch_val > sample_val) sample_val = ch_val;
-                }
-                value_t slope = (sample_val - m_avg_amp) / i;
-                if (slope >= m_max_slope) { // must be >=, otherwise clipping causes excessive computation
-                    m_max_slope_pos = (m_pos + i) % m_samples;
-                    m_max_slope = slope;
-                    m_max_slope_val = sample_val;
-                }
-            }
-        } else {
-            // only chance of higher slope is the new sample
-            // recomputing max_slope isn't really necessary...
-            m_max_slope = (m_max_slope_val - m_avg_amp) / ((m_max_slope_pos - m_pos + m_samples) % m_samples);
-            // ...but it doesn't take long and has a small effect.
+		// swap buf[pos] with user_buf[user_pos], scaling user[buf] by
+		// multiplier and finding max of the new sample
+		value_t new_val = 0;
+		for(size_t ch = 0; ch < channels; ++ch) {
+			value_t in = in_bufs[ch][user_pos];
+			out_bufs[ch][user_pos] = bufs[ch][pos] * multiplier;
+			bufs[ch][pos] = in;
+			if(VLEVEL_ABS(in) > new_val) new_val = VLEVEL_ABS(in);
+		}
 
-            value_t slope = (new_val - m_avg_amp) / (m_samples - 1);
+		pos = (pos + 1) % samples; // now pos is the oldest, new one is pos-1
 
-            if (slope >= m_max_slope) { // probably needs to be >= for same reason as above
-                m_max_slope_pos = (m_pos - 1) % m_samples;
-                m_max_slope = slope;
-                m_max_slope_val = new_val;
-            }
-        }
-    }
+		avg_amp += max_slope;
+
+		if(pos == max_slope_pos) {
+			// recompute (this is expensive)
+			max_slope = -HUGE_VAL;
+			for(size_t i = 1; i < samples; ++i) {
+				value_t sample_val = 0;
+				for(size_t ch = 0; ch < channels; ++ch) {
+					value_t ch_val = VLEVEL_ABS(bufs[ch][(pos + i) % samples]);
+					if(ch_val > sample_val) sample_val = ch_val;
+				}
+				value_t slope = (sample_val - avg_amp) / i;
+				// must be >=, otherwise clipping causes excessive computation
+				// TODO: maybe optimize - just save i, then compute slope, pos, and val only once later.
+				if(slope >= max_slope) {
+					max_slope_pos = (pos + i) % samples;
+					max_slope = slope;
+					max_slope_val = sample_val;
+				}
+			}
+		} else {
+			// only chance of higher slope is the new sample
+			// recomputing max_slope isn't really necessary...
+			max_slope = (max_slope_val - avg_amp) / ((max_slope_pos - pos + samples) % samples);
+			// ...but it doesn't take long and has a small effect.
+
+			value_t slope = (new_val - avg_amp) / (samples - 1);
+
+			// probably needs to be >= for same reason as above
+			if(slope >= max_slope) {
+				max_slope_pos = (pos - 1) % samples;
+				max_slope = slope;
+				max_slope_val = new_val;
+			}
+		}
+	}
 }
--- volumeleveler.h
+++ volumeleveler.h
@@ -1,23 +1,24 @@
-// Copyright 2003 Tom Felker
+// This file is part of VLevel, a dynamic volume normalizer.
 //
-// This file is part of VLevel.
+// Copyright 2003 Tom Felker <tcfelker@mtco.com>
 //
-// VLevel is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
 //
-// VLevel is distributed in the hope that it will be useful, but
+// This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with VLevel; if not, write to the Free Software Foundation,
-// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
 
 // volumeleveler.h - declares the VolumeLeveler class
-
+//
 // A note on terminology: atoms are indivual value_t values; samples
 // are one or more atoms, for example, for stereo, a sample has 2
 // atoms, and channels is 2.
@@ -31,78 +32,78 @@
 
 class VolumeLeveler {
 public:
-    // constructs and destructs a VolumeLeveler with a length of l
-    // samples with c channels each, an effect strength of s and a
-    // maximum multiplier of m
-    VolumeLeveler(size_t l = 44100, size_t c = 2, value_t s = .8, value_t m = 25);
-    ~VolumeLeveler();
+	// constructs and destructs a VolumeLeveler with a length of l
+	// samples with c channels each, an effect strength of s and a
+	// maximum multiplier of m
+	VolumeLeveler(size_t l = 44100, size_t c = 2, value_t s = .8, value_t m = 25);
+	~VolumeLeveler();
 
-    // Reallocates a buffer of l samples and c channels (contents are
-    // lost)
-    void SetSamplesAndChannels(size_t l, size_t c);
+	// Reallocates a buffer of l samples and c channels (contents are
+	// lost)
+	void SetSamplesAndChannels(size_t l, size_t c);
 
-    // set and get the strength (between 0 and 1) (set doesn't affect
-    // the buffer) if undo is true, vlevel will do the exact opposite,
-    // so you can remove a vlevel.
-    void SetStrength(value_t s);
+	// set and get the strength (between 0 and 1) (set doesn't affect
+	// the buffer) if undo is true, vlevel will do the exact opposite,
+	// so you can remove a vlevel.
+	void SetStrength(value_t s);
 
-    // set and get the max multiplier (set doesn't affect the buffer)
-    void SetMaxMultiplier(value_t m);
+	// set and get the max multiplier (set doesn't affect the buffer)
+	void SetMaxMultiplier(value_t m);
 
-    // get stuff
-    inline size_t GetSamples() { return m_samples; };
-    inline size_t GetChannels() { return m_channels; };
-    inline value_t GetStrength() { return m_strength; };
-    inline value_t GetMaxMultiplier() { return m_max_multiplier; };
-    inline size_t GetSilence() { return m_silence; };
+	// get stuff
+	inline size_t GetSamples() { return samples; };
+	inline size_t GetChannels() { return channels; };
+	inline value_t GetStrength() { return strength; };
+	inline value_t GetMaxMultiplier() { return max_multiplier; };
+	inline size_t GetSilence() { return silence; };
 
-    // get stats
-    value_t GetMultiplier();
+	// get stats
+	value_t GetMultiplier();
 
-    // fills the buffers with silence
-    void Flush();
+	// fills the buffers with silence
+	void Flush();
 
-    // replaces raw with processed, returns how many samples are
-    // residual silence from when the buffers were empty.
-    size_t Exchange(value_t **in_buf, value_t **out_buf, size_t in_samples);
+	// replaces raw with processed, returns how many samples are
+	// residual silence from when the buffers were empty.
+	size_t Exchange(value_t **in_buf, value_t **out_buf, size_t in_samples);
 
 private:
-    //void Exchange_1(value_t **in_buf, value_t **out_buf, size_t in_samples);
-    //void Exchange_2(value_t **in_buf, value_t **out_buf, size_t in_samples);
-    void Exchange_n(value_t **in_buf, value_t **out_buf, size_t in_samples);
+	//void Exchange_1(value_t **in_buf, value_t **out_buf, size_t in_samples);
+	//void Exchange_2(value_t **in_buf, value_t **out_buf, size_t in_samples);
+	void Exchange_n(value_t **in_buf, value_t **out_buf, size_t in_samples);
 
-    // the buffer
-    value_t **m_bufs;
+	// the buffer
+	value_t **bufs;
 
-    // the length of the buffers
-    size_t m_samples;
+	// the length of the buffers
+	size_t samples;
 
-    // the number of channels
-    size_t m_channels;
+	// the number of channels
+	size_t channels;
 
-    // the strength of the effect (between 0 and 1)
-    value_t m_strength;
+	// the strength of the effect (between 0 and 1)
+	value_t strength;
 
-    // the maximum value by which a sample will be scaled
-    value_t m_max_multiplier;
+	// the maximum value by which a sample will be scaled
+	value_t max_multiplier;
 
-    // the amount of silence (data that wasn't input) left in the buffer (samples).
-    size_t m_silence;
+	// the amount of silence (data that wasn't input) left in the buffer (samples).
+	size_t silence;
 
-    // position about to be returned (samples)
-    size_t m_pos;
+	// position about to be returned (samples)
+	size_t pos;
 
-    // position of the max slope (samples)
-    size_t m_max_slope_pos;
+	// position of the max slope (samples)
+	size_t max_slope_pos;
 
-    // the current "blanket" amplitude
-    value_t m_avg_amp;
+	// the current "blanket" amplitude
+	value_t avg_amp;
 
-    // the maximum slope
-    value_t m_max_slope;
+	// the maximum slope
+	value_t max_slope;
 
-    // the value at the maximum slope
-    value_t m_max_slope_val;
+	// the value at the maximum slope
+	value_t max_slope_val;
 };
 
 #endif // ndef VOLUMELEVELER_H
